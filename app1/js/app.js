6dc3cc76513d2b2471f1184f4c897b547b19ba61
Josh Whiting <yetanotherjosh@gmail.com>
2013-09-04 12:20:42 -0700

    first commit of my code i've been working on for a while now trying a variety of things

---
+++ app1/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ngStorage']).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/tasks', {templateUrl: 'partials/partial1.html', controller: 'TaskListCtrl'});
+    $routeProvider.when('/new', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.otherwise({redirectTo: '/tasks'});
+  }]);
---
+++ app1/js/controllers.js
@@ -0,0 +1,80 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
+
+function TaskListCtrl($scope, $localStorage) {
+  $scope.$storage = $localStorage.$default({ foo: 42 });
+  $scope.tasks = [
+    { "name": "do stuff 1", "owner": "joe" },
+    { "name": "do stuff 2", "owner": "bob" },
+    { "name": "do stuff 3", "owner": "alice" },
+    { "name": "do stuff 4", "owner": "marney" },
+  ];
+  $scope.sortOrder = 1;
+  $scope.sortBy = 'name';
+
+  $scope.doSort = function() {
+    var self = this;
+    self.tasks.sort(function(a,b){
+      if (a[self.sortBy] > b[self.sortBy]) return self.sortOrder;
+      if (a[self.sortBy] < b[self.sortBy]) return (-1 * self.sortOrder);
+      return 0;
+    });
+    var props = ['name','owner'];
+    self.sortButtonValue = {};
+    for (var i = 0; i < props.length; i++) {
+      var prop = props[i];
+      if (self.sortBy == prop) {
+        self.sortButtonValue[prop] = (self.sortOrder > 0 ? "&darr;&darr;" : "&uarr;&uarr;");
+      } else {
+        self.sortButtonValue[prop] = "&darr;";
+      }
+    }
+  }
+  $scope.sortByProp = function(prop) {
+    if ($scope.sortBy == prop) {
+      $scope.sortOrder = -1 * $scope.sortOrder;
+    } else {
+      $scope.sortBy = prop;
+    }
+    $scope.doSort();
+  }
+
+  $scope.doSort();
+
+};
+
+
+function ModalTestCtrl($scope, $timeout) {
+
+  $scope.openIt = function() {
+    console.log("open modal");
+    var self=this;
+    self.activeModal = false;
+    self.inModal = true;
+    $timeout(function(){
+      console.log("open modal timeout");
+      self.activeModal = true;
+    });
+  };
+
+  $scope.closeIt = function() {
+    console.log("close modal");
+    var self=this;
+    self.activeModal = false;
+    $timeout(function(){
+      console.log("close modal timeout");
+      self.inModal = false;
+    }, 200);
+  };
+
+
+};
