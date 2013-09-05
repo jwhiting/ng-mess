ddd25b3bbbe537a4fdbd5d63e8fcb863ed431aed
Josh Whiting <yetanotherjosh@gmail.com>
2013-09-04 22:55:50 -0700

    abandonend

--- app1/js/directives.js
+++ app1/js/directives.js
@@ -59,35 +59,30 @@ angular.module('myApp.directives', []).
     }
   }).
 
-  directive('betterXhrModal', ['$compile','$timeout','$document','$http','spinStates',function($compile,$timeout,$document,$http,spinStates){
+  directive('betterXhrModal', ['$compile','$timeout','$document','$http',function($compile,$timeout,$document,$http){
     return {
       link: function(scope, element, attrs) {
         function loadModal() {
-          //var spinnerOpts = {
-          //  lines: 9, // The number of lines to draw
-          //  length: 0, // The length of each line
-          //  width: 3, // The line thickness
-          //  radius: 5, // The radius of the inner circle
-          //  corners: 1, // Corner roundness (0..1)
-          //  rotate: 0, // The rotation offset
-          //  direction: 1, // 1: clockwise, -1: counterclockwise
-          //  color: '#000', // #rgb or #rrggbb or array of colors
-          //  speed: 1.5, // Rounds per second
-          //  trail: 33, // Afterglow percentage
-          //  shadow: false, // Whether to render a shadow
-          //  hwaccel: false, // Whether to use hardware acceleration
-          //  className: 'spinner', // The CSS class to assign to the spinner
-          //  zIndex: 2e9, // The z-index (defaults to 2000000000)
-          //  top: 'auto', // Top position relative to parent in px
-          //  left: 'auto' // Left position relative to parent in px
-          //};
-          //var spinner = new Spinner(spinnerOpts).spin(element[0]);
-          //element.addClass('cleartext');
-
-          var spinKey = 'modal-'+Math.random();
-          spinStates.register(element[0],spinKey);
-          spinStates.setActive(spinKey);
-
+          var spinnerOpts = {
+            lines: 9, // The number of lines to draw
+            length: 0, // The length of each line
+            width: 3, // The line thickness
+            radius: 5, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            color: '#000', // #rgb or #rrggbb or array of colors
+            speed: 1.5, // Rounds per second
+            trail: 33, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            top: 'auto', // Top position relative to parent in px
+            left: 'auto' // Left position relative to parent in px
+          };
+          var spinner = new Spinner(spinnerOpts).spin(element[0]);
+          element.addClass('cleartext');
           var fetchPromise = $http.get(attrs.betterXhrModal);
           fetchPromise.then(function(result){
             console.log("better xhr modal got remote modal content",result);
@@ -119,15 +114,11 @@ angular.module('myApp.directives', []).
             var body = $document.find('body').eq(0); // todo: use root app element instead?
             body.append(modalDomEl);
             modalScope.openIt();
-            //spinner.stop();
-            //element.removeClass('cleartext');
-            spinStates.setInactive(spinKey);
-            spinStates.unregister(element);
+            spinner.stop();
+            element.removeClass('cleartext');
           }, function(error){
-            //spinner.stop();
-            //element.removeClass('cleartext');
-            spinStates.setInactive(spinKey);
-            spinStates.unregister(element);
+            spinner.stop();
+            element.removeClass('cleartext');
             console.log("better xhr modal failed to get modal content", error);
             //... eventually i want the modal directive to implement error handling with a specific error modal
           })
